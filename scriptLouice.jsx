40fa6ab0088209c457ad061c1046ad4f376d1625
Louice Danielsson <louice.danielsson@gmail.com>
2017-04-27 16:26:40 +0200

    added functioning Cat Fact React Component

--- scriptLouice.jsx
+++ scriptLouice.jsx
@@ -1,19 +1,64 @@
-window.addEventListener('load', function(event) {
-  
-  
-  console.log('before');
-  let ajax = new XMLHttpRequest();
-  let url = 'http://catfacts-api.appspot.com/api/facts';
-  ajax.open('get', url);
-  ajax.onreadystatechange = function() {
-    if(ajax.status == 200 && ajax.readyState == 4) {
-      let response = JSON.parse(ajax.responseText)
-      console.log('Response: ', response);
-    }
+
+
+//baskomponent, skall rendera componenterna:
+//Weather
+//CatFact
+//Yt
+//Lists
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentFact: ''
+    };
+    this.showFact = this.showFact.bind(this);
   }
-  ajax.send();
+
+  showFact() {
+    console.log('before');
+    let ajax = new XMLHttpRequest();
+    let url = 'http://catfacts-api.appspot.com/api/facts';
+    ajax.open('get', url);
+    ajax.onreadystatechange = (function() {
+      if(ajax.status == 200 && ajax.readyState == 4) {
+        let response = JSON.parse(ajax.responseText)
+        let fact = response.facts[0];
+        console.log(`fact: ${fact}`);
+        this.setState({
+          currentFact: fact
+        })
+      }
+    }).bind(this);
+    ajax.send();
+    console.log('after');
+
+  }
   
-  console.log('after');
-  
-  
-});
+
+
+
+  render() {
+    return(
+      <CatFact handleClick={this.showFact} currentFact={this.state.currentFact} />
+    )
+  }
+}
+
+class CatFact extends React.Component {
+  render() {
+    return(
+      <div id="facts">
+        <h3>Facts</h3>
+        <button onClick={this.props.handleClick}>Show something else</button>
+        <p>{this.props.currentFact}</p>
+      </div>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
