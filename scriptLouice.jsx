1c0ad6565c3e5e99748facefa89a82d150ac23e2
Louice Danielsson <louice.danielsson@gmail.com>
2017-04-27 10:55:30 +0200

    added cat api for test

---
+++ scriptLouice.jsx
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(event) {
+  console.log('before');
+  let ajax = new XMLHttpRequest();
+  let url = 'http://catfacts-api.appspot.com/api/facts';
+  ajax.open('get', url);
+  ajax.onreadystatechange = function() {
+    if(ajax.status == 200 && ajax.readyState == 4) {
+      let response = JSON.parse(ajax.responseText)
+      console.log(`Response: ${response}`);
+    }
+  }
+  ajax.send();
+  
+  console.log('after');
+});
