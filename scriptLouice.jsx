99d6178376c6e8284485df0750d894d8ba611dc8
Louice Danielsson <louice.danielsson@gmail.com>
2017-04-27 12:50:32 +0200

    solved cat api prop w cors extension

--- scriptLouice.jsx
+++ scriptLouice.jsx
@@ -1,4 +1,6 @@
 window.addEventListener('load', function(event) {
+  
+  
   console.log('before');
   let ajax = new XMLHttpRequest();
   let url = 'http://catfacts-api.appspot.com/api/facts';
@@ -6,10 +8,12 @@ window.addEventListener('load', function(event) {
   ajax.onreadystatechange = function() {
     if(ajax.status == 200 && ajax.readyState == 4) {
       let response = JSON.parse(ajax.responseText)
-      console.log(`Response: ${response}`);
+      console.log('Response: ', response);
     }
   }
   ajax.send();
   
   console.log('after');
+  
+  
 });
