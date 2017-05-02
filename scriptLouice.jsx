f56908592dc95a42e052a423d0194a2a88300005
Louice Danielsson <louice.danielsson@gmail.com>
2017-05-02 10:30:19 +0200

    removed static html lists

--- scriptLouice.jsx
+++ scriptLouice.jsx
@@ -1,20 +1,16 @@
-
-
-//baskomponent, skall rendera componenterna:
-//Weather
-//CatFact
-//Yt
-//Lists
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentFact: ''
+      currentFact: '',
+      isLoggedIn: false
     };
     this.showFact = this.showFact.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.updateLogInStatus = this.updateLogInStatus.bind(this);
   }
 
-  showFact() {
+  showFact(event) {
     console.log('before');
     let ajax = new XMLHttpRequest();
     let url = 'http://catfacts-api.appspot.com/api/facts';
@@ -31,16 +27,42 @@ class App extends React.Component {
     }).bind(this);
     ajax.send();
     console.log('after');
+  } //ajax cat fact
+  
+  logIn(event) {
+    let provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+      //om inloggning lyckas
+      console.log('logging in...');
+      if(result.user.displayName == 'Louice Danielsson') {
+        console.log('sign in ok');
+        updateLogInStatus();
+      }
+    }).catch( function(error) {
+      //om inloggningen misslyckas
+      console.log(`Error: ${error.code}, ${error.message}`);
+    });
   }
   
-
-
-
+  updateLogInStatus() {
+    console.log('update körs');
+    this.setState({
+      isLoggedIn: true
+    });
+  }
   render() {
     return(
-      <CatFact handleClick={this.showFact} currentFact={this.state.currentFact} />
+      <div>
+        <CatFact 
+          handleClick={this.showFact} 
+          currentFact={this.state.currentFact} />
+        <Admin 
+          handleClick={this.logIn} 
+          status={this.updateLogInStatus} />
+      </div>
     )
-  }
+  } 
 }
 
 class CatFact extends React.Component {
@@ -50,6 +72,16 @@ class CatFact extends React.Component {
         <h3>Facts</h3>
         <button onClick={this.props.handleClick}>Show something else</button>
         <p>{this.props.currentFact}</p>
+      </div>
+    )
+  }
+}
+
+class Admin extends React.Component {
+  render() {
+    return(
+      <div id="admin">
+        <button onClick={this.props.handleClick}>Admin</button>
       </div>
     )
   }
